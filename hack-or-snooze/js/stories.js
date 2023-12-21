a80ca30e4fb0486c6b7c87ab38214e8861c504f7
maahin <haquemaahin@gmail.com>
2023-12-20 21:13:46 -0500

    added files

---
+++ hack-or-snooze/js/stories.js
@@ -0,0 +1,145 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story, deleteButton = false) {
+  // console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+        ${deleteButtonHTML(deleteButton)}
+        ${favStarHTML(story)}
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+
+function favStarHTML(story){
+  if(currentUser){
+    if(isFavorite(story)){
+      return `<span class="star"> <i class="fas fa-star"></i></span>`
+    }
+    else{
+      return `<span class="star"> <i class="far fa-star"></i></span>`
+    }
+  }
+  else{
+    return ""
+  }
+  
+}
+
+function deleteButtonHTML(includeDelte){
+  if(includeDelte) return `<span><i class="fas fa-trash"></i></span>`;
+  else return "";
+  
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+function putFavoriteStoriesOnPage(){
+  $favStoriesList.empty();
+  
+  for (let story of currentUser.favorites){
+    const $story = generateStoryMarkup(story);
+    $favStoriesList.append($story);
+  }
+
+  $favStoriesList.show();
+}
+
+function putOwnStoriesOnPage(){
+  $ownStoriesList.empty();
+
+  for (let story of currentUser.ownStories){
+    const $story = generateStoryMarkup(story,true);
+    $ownStoriesList.append($story);
+  }
+
+  $ownStoriesList.show();
+}
+
+$("#submit-form").on("click","button", async ()=>{
+  const author = $("#submit-form #create-author").val();
+  const title = $("#submit-form #create-title").val();
+  const url = $("#submit-form #create-url").val();
+
+  //empty the input
+  $("#submit-form #create-author").val("");
+  $("#submit-form #create-title").val("");
+  $("#submit-form #create-url").val("");
+
+  await storyList.addStory(currentUser,{author,title,url});
+  console.log("successfully added a story");
+
+  putStoriesOnPage();
+
+  //$signupForm.hide()
+  $submitForm.hide();
+  //$("#submit-form").css("display","none");
+});
+
+$("body").on("click",".fa-star",(event)=>{ //far is empty fas is filled
+  const storyId = $(event.target).closest("li").attr('id');
+
+  const story = storyList.stories.filter((savedStories)=>{
+    return savedStories.storyId === storyId;
+  });
+
+  $(event.target).toggleClass("far fas");
+  if($(event.target).hasClass("fas")){
+    addFavorite(story[0]);
+  }
+  else{
+    removeFavorite(story[0]);
+  }
+  
+})
+
+$ownStoriesList.on("click", ".fa-trash", async (event)=>{
+  const storyId = $(event.target).closest("li").attr("id");
+  console.log(storyId);
+  await storyList.deleteStory(currentUser,storyId);
+
+  putOwnStoriesOnPage();
+
+})
+
---
+++ hack-or-snooze/js/user.js
@@ -0,0 +1,164 @@
+"use strict";
+
+// global to hold the User instance of the currently-logged-in user
+let currentUser;
+
+/******************************************************************************
+ * User login/signup/login
+ */
+
+/** Handle login form submission. If login ok, sets up the user instance */
+
+async function login(evt) {
+  console.debug("login", evt);
+  evt.preventDefault();
+
+  // grab the username and password
+  const username = $("#login-username").val();
+  const password = $("#login-password").val();
+
+  // User.login retrieves user info from API and returns User instance
+  // which we'll make the globally-available, logged-in user.
+  currentUser = await User.login(username, password);
+
+  $loginForm.trigger("reset");
+
+  saveUserCredentialsInLocalStorage();
+  updateUIOnUserLogin();
+  $loginForm.hide();
+  $signupForm.hide();
+  
+}
+
+$loginForm.on("submit", login);
+
+/** Handle signup form submission. */
+
+async function signup(evt) {
+  console.debug("signup", evt);
+  evt.preventDefault();
+
+  const name = $("#signup-name").val();
+  const username = $("#signup-username").val();
+  const password = $("#signup-password").val();
+
+  // User.signup retrieves user info from API and returns User instance
+  // which we'll make the globally-available, logged-in user.
+  currentUser = await User.signup(username, password, name);
+
+  saveUserCredentialsInLocalStorage();
+  updateUIOnUserLogin();
+
+  $signupForm.trigger("reset");
+}
+
+$signupForm.on("submit", signup);
+
+/** Handle click of logout button
+ *
+ * Remove their credentials from localStorage and refresh page
+ */
+
+function logout(evt) {
+  console.debug("logout", evt);
+  localStorage.clear();
+  location.reload();
+}
+
+$navLogOut.on("click", logout);
+
+/******************************************************************************
+ * Storing/recalling previously-logged-in-user with localStorage
+ */
+
+/** If there are user credentials in local storage, use those to log in
+ * that user. This is meant to be called on page load, just once.
+ */
+
+async function checkForRememberedUser() {
+  console.debug("checkForRememberedUser");
+  const token = localStorage.getItem("token");
+  const username = localStorage.getItem("username");
+  if (!token || !username) return false;
+
+  // try to log in with these credentials (will be null if login failed)
+  currentUser = await User.loginViaStoredCredentials(token, username);
+}
+
+/** Sync current user information to localStorage.
+ *
+ * We store the username/token in localStorage so when the page is refreshed
+ * (or the user revisits the site later), they will still be logged in.
+ */
+
+function saveUserCredentialsInLocalStorage() {
+  console.debug("saveUserCredentialsInLocalStorage");
+  if (currentUser) {
+    localStorage.setItem("token", currentUser.loginToken);
+    localStorage.setItem("username", currentUser.username);
+  }
+}
+
+/**
+ * 
+ * Saves favorite stories 
+ * 
+ */
+
+async function addFavorite(story){
+  const response = await axios({
+    url: `https://hack-or-snooze-v3.herokuapp.com/users/${currentUser.username}/favorites/${story.storyId}`,
+    method: "POST",
+    data: { token: currentUser.loginToken },
+  });
+
+  currentUser.favorites.push(story);
+  console.log("added fav");
+}
+
+async function removeFavorite(story){
+  const response = await axios({
+    url: `https://hack-or-snooze-v3.herokuapp.com/users/${currentUser.username}/favorites/${story.storyId}`,
+    method: "DELETE",
+    data: { token: currentUser.loginToken },
+  })
+
+  currentUser.favorites = currentUser.favorites.filter((favoriteStory)=>{
+    return favoriteStory.storyId !== story.storyId;
+  });
+
+  console.log("removed fav")
+ 
+}
+
+/******************************************************************************
+ * General UI stuff about users
+ */
+
+/** When a user signs up or registers, we want to set up the UI for them:
+ *
+ * - show the stories list
+ * - update nav bar options for logged-in user
+ * - generate the user profile part of the page
+ */
+
+function updateUIOnUserLogin() {
+  console.debug("updateUIOnUserLogin");
+
+  putStoriesOnPage();
+  $allStoriesList.show();
+  updateNavOnLogin();
+  $("#logged-navbar-links").css("display","block");
+}
+
+function isFavorite(story){
+  return currentUser.favorites.some((favoriteStory)=>{
+    return favoriteStory.storyId === story.storyId; 
+  })
+}
+
+function isOwnStory(story){
+  return currentUser.ownStories.some((ownStory)=>{
+    return ownStory.storyId === story.storyId; 
+  })
+}
