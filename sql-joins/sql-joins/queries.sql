554d5818e1067dc9566ccd9d09f6e6104c0d0aef
maahin <haquemaahin@gmail.com>
2024-01-28 21:58:49 -0500

    added files

---
+++ sql-joins/sql-joins/queries.sql
@@ -0,0 +1,8 @@
+SELECT * FROM owners FULL OUTER JOIN vehicles v ON owners.id = vehicles.owner_id;
+
+SELECT first_name, last_name, COUNT(owner_id) FROM owners JOIN vehicles ON owners.id = vehicles.owner_id 
+    GROUP BY (first_name, last_name) ORDER BY first_name;
+
+SELECT first_name, last_name, ROUND(AVG(price)) AS average_price, COUNT(owner_id) FROM owners 
+    JOIN vehicles ON owners.id = vehicles.owner_id GROUP BY (first_name, last_name) HAVING 
+    COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000 ORDER BY first_name DESC;
